fd3bd6511329e0b53c658e7e5001b9793f574bd6
Galih Rizky Fahrezy <galihrizkyfahrezy@gmail.com>
2024-06-18 07:47:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$test01 = "halo, nama saya adalah galih homina.";
+
+$row=5;
+$col=5;
+
+for($i=0;$i<($row);$i++)
+{
+    for($j=0;$j<$col;$j++)
+    {
+        echo "*";
+    }
+    echo "<br>";
+}
+
+?>
